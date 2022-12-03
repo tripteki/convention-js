ab7d0bd0d8fe27c0bef444a442eb3ce4d61ce0d8
hsbmaulana <hsbmaulana.iso@protonmail.com>
2022-12-03 17:50:08 +0700

    build(core): revamp

--- index.js
+++ index.js
@@ -2,47 +2,81 @@
 
 module.exports =
 {
+    "overrides": [
+
+        {
+            "files": [ "**/*.yaml", "**/*.yml", ],
+            // "plugins": [],
+            "extends": [
+
+                "plugin:yml/recommended",
+            ],
+            "parser": "yaml-eslint-parser",
+            // "parserOptions": {},
+            // "env": {},
+        },
+        {
+            "files": [ "**/*.json", ],
+            "plugins": [ "json", ],
+            "extends": [
+
+                "plugin:json/recommended",
+            ],
+            // "parser": "",
+            // "parserOptions": {},
+            // "env": {},
+        },
+        {
+            "files": [ "**/*.vue", ],
+            "plugins": [ "vue", ],
+            "extends": [
+
+                "plugin:vue/recommended",
+            ],
+            "parser": "vue-eslint-parser",
+            // "parserOptions": {},
+            // "env": {},
+        },
+        {
+            "files": [ "**/*.jsx", ],
+            "plugins": [ "react", "react-native", ],
+            "extends": [
+
+                "plugin:react/recommended",
+                "plugin:react-native/all",
+            ],
+            // "parser": "",
+            "parserOptions": {
+
+                "ecmaFeatures": { "jsx": true, },
+            },
+            "env": {
+
+                "react-native/react-native": true,
+            },
+        },
+        { "files": [ "*.php", "*.js", "*.ts", "*.py", "*.java", ], },
+    ],
+
     "plugins": [
 
         "jsdoc",
         "@typescript-eslint",
-        "vue",
-        "react",
-        "react-native",
     ],
 
     "extends": [
 
         "eslint:recommended",
-
         "plugin:jsdoc/recommended",
         "plugin:@typescript-eslint/recommended",
-        "plugin:vue/recommended",
-        "plugin:react/recommended",
-        //"plugin:react-native/all",
     ],
 
-    "env": {
-
-        "react-native/react-native": true,
-    },
-
-    //"parser": [
-
-        //"@typescript-eslint/parser",
-    //],
-
-    "parserOptions": {
-
-        "ecmaVersion": "latest",
-        "ecmaFeatures": { "jsx": true, },
-    },
+    "parser": "@typescript-eslint/parser",
 
 
 
     "root": true,
     "ignorePatterns": [ "**/.git/*", "**/vendor/*", "**/node_modules/*", "**/.gradle/*", ],
-    "overrides": [ { "files": [ "*.php", "*.js", "*.ts", "*.py", "*.java", ], }, ],
 
     "rules": {
 
@@ -52,6 +86,7 @@ module.exports =
         "indent": [ "error", 4, ],
         "no-multi-spaces": "error",
         "brace-style": [ "error", "allman", { "allowSingleLine": false, }, ],
+        "@typescript-eslint/no-var-requires": "off",
         "no-constant-condition": "error",
         "space-unary-ops": "error",
         "keyword-spacing": [ "error", { "before": true, "after": true, }, ],
@@ -62,10 +97,18 @@ module.exports =
         "array-bracket-spacing": [ "error", "always", ],
         "key-spacing": [ "error", { "beforeColon": false, "afterColon": true, }, ],
         "func-call-spacing": [ "error", "always", ],
+        "newline-after-var": [ "error", "always", ],
         "function-paren-newline": [ "error", "never", ],
 
         "quotes": [ "error", "double", ],
-        "comma-dangle": [ "error", "always", ],
+        "comma-dangle": [ "error", {
+
+            "arrays": "always",
+            "objects": "always",
+            "imports": "always",
+            "exports": "always",
+            "functions": "never",
+        }, ],
         "semi": [ "error", "always", ],
         "semi-style": [ "error", "last", ],
         "semi-spacing": [ "error", { "before": false, "after": true, }, ],
