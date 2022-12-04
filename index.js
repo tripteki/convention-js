dad6286411846f52e9c4abf0254198e5107d4d58
Hasby Maulana <hsbmaulana.iso@protonmail.com>
2022-12-04 07:34:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ module.exports =
         "keyword-spacing": [ "error", { "before": true, "after": true, }, ],
         "padded-blocks": [ "error", "never", ],
         "padding-line-between-statements": [ "error", { "blankLine": "always", "prev": "*", "next": [ "return", ], }, { "blankLine": "always", "prev": [ "const", "let", "var", ], "next": "*", }, { "blankLine": "any", "prev": [ "const", "let", "var", ], "next": [ "const", "let", "var", ], }, ],
-        "space-before-function-parentheses": [ "error", "always", ],
+        "space-before-function-paren": [ "error", "always", ],
         "space-before-blocks": [ "error", "always", ],
         "object-curly-newline": [ "error", "always", ],
         "object-curly-spacing": [ "error", "always", ],
